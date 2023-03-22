73f87c31769059a8c054eca59bb84e7638368fdf
sujon <smsujonrahman1999@gmail.com>
2023-03-22 12:37:19 +0600

    getting started

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      padding:{
+        '15':'60px',
+      },
+      margin:{
+        '15':'60px',
+      },
+      boxShadow: {
+        '3xl': '0px 8px 20px rgba(0, 0, 0, 0.06)',
+      },
+      fontSize: {
+        '3xxl': ['32px', '36px'],
+        '4xxl': ['40px', '48px'],
+        '6xxl': ['60px', '68px'],
+      },
+      colors:{
+        'PrimaryYellow':'#F6C60D',
+        'PrimaryBlue':'#00AAEF',
+        'PrimaryDark':'#6F6B80',
+        'SecondaryGrey':'#797979',
+        'SecondaryBleue':'#6C77E5',
+        'ThirdPampus':'#B38180',
+        'ThirdGrey':'#F5F5F5',
+      },
+      backgroundImage:{
+        'InvestmentBgc': "url('/public/assets/images/investment.jpg')",
+      },
+      borderRadius: {
+        'ss': '5px',
+      },
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif',],
+        'Poppins': ['Poppins', 'sans-serif',],
+
+      },
+      screens: {
+        'llg': '1180px',
+      },
+      backgroundPosition:{
+        'TopLeft': 'top 15px left 10%',
+        'sujon': 'center center',
+      },
+    },
+  },
+  plugins: [],
+}
